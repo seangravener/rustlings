11b7144dfd957f63cd25660da60721fb7b04ac6b
Sean Gravener <sean@gravener.net>
2023-02-08 20:52:44 -0500

    Complete exercise

--- exercises/functions/functions2.rs
+++ exercises/functions/functions2.rs
@@ -1,14 +1,18 @@
 // functions2.rs
 // Execute `rustlings hint functions2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     call_me(3);
 }
 
-fn call_me(num:) {
+fn call_me(num: u8) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
+    }
+}
+
+fn call_me_again(num: u8) {
+    for i in 0..num {
+        println!("Ring! {}", i + 1)
     }
 }
--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -1,14 +1,12 @@
 // functions3.rs
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    call_me();
+    call_me(8)
 }
 
-fn call_me(num: u32) {
+fn call_me(num: u8) {
     for i in 0..num {
-        println!("Ring! Call number {}", i + 1);
+        println!("Ring! Call number {}", i + 1)
     }
 }
